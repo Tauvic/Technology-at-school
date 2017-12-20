1c2ab4cec88feff9c162c757e8a87da7d421269c
Tauvic Ritter <tauvicr@gmail.com>
2017-12-20 09:13:10 +0100

    Moved code

---
+++ Robots/Makeblock/develop/project_SensorArray/MeLineFollowArray.h
@@ -0,0 +1,36 @@
+#ifndef MeLineFollowArray_h
+#define MeLineFollowArray_h
+
+/* Includes ------------------------------------------------------------------*/
+#include <stdint.h>
+#include <stdbool.h>
+#include <Arduino.h>
+#include "MeConfig.h"
+
+#ifdef ME_PORT_DEFINED
+#include "MePort.h"
+#endif /* ME_PORT_DEFINED */
+
+class MeLineFollowArray : public MePort
+{    
+	public:
+    //direction capabilities
+    enum direction {can_nowhere,can_forward,can_left,can_right,can_left_right,can_left_right_forward};
+    MeLineFollowArray(void);
+    MeLineFollowArray(uint8_t port);  
+    MeLineFollowArray(uint8_t pin1,uint8_t pin2);  
+    void setpin(uint8_t pin1,uint8_t pin2);
+    bool readSensor();
+    int8_t getPosition();
+    direction getDirection();
+		uint8_t getRawValue();
+ private:
+    uint8_t pin1=0; 
+    uint8_t pin2=0;    
+		uint8_t raw=0;
+    int8_t weighted=0;
+    bool isValidLine(uint8_t raw);
+		String fillZero(String bin);
+};
+
+#endif
